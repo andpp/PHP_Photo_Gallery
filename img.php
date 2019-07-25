a55e6a8f5c6b9873d279dc1a0b03c18f6edd58fb
Andrey Perminov <andrey.ppp@gmail.com>
2019-07-25 15:27:34 -0700

    Check for valid extension for processed image files

--- img.php
+++ img.php
@@ -4,36 +4,41 @@
     require_once('php_image_magician.php');
 
     $name = $_GET["f"];
+    $extensions    = array(".jpg",".png",".gif",".JPG",".PNG",".GIF");
+    $ext = strrchr($name, '.');
+    if(in_array($ext, $extensions)) {
 
-    // Open JPG image
-    $magicianObj = new imageLib($name);
+        // Open JPG image
+        $magicianObj = new imageLib($name);
 
-    // Resize to best fit then crop
-    $magicianObj -> resizeImage(1300, 1000, 'auto');
+        // Resize to best fit then crop
+        $magicianObj -> resizeImage(1300, 1000, 'auto');
 
-
-    $exifArray = $magicianObj -> getExif();
+	$exifArray = $magicianObj -> getExif();
 //  1: 'rotate(0deg)',
 //  3: 'rotate(180deg)',
 //  6: 'rotate(90deg)',
 //  8: 'rotate(270deg)'
 
-    switch ($exifArray['orientation']) {
-        case 1:
-            // $magicianObj -> rotate(0);
-             break;
-        case 3:
-             $magicianObj -> rotate(180);
-             break;
-        case 6:
-             $magicianObj -> rotate(90);
-             break;
-        case 8:
-             $magicianObj -> rotate(270);
-             break;
-    }
+        switch ($exifArray['orientation']) {
+            case 1:
+                 // $magicianObj -> rotate(0);
+                 break;
+            case 3:
+                 $magicianObj -> rotate(180);
+                 break;
+            case 6:
+                 $magicianObj -> rotate(90);
+                 break;
+            case 8:
+                 $magicianObj -> rotate(270);
+                 break;
+        }
 
-    // Save resized image as a PNG
-    $magicianObj -> displayImage('jpg', 85) ;
+        // Save resized image as a PNG
+        $magicianObj -> displayImage('jpg', 85) ;
+   } else {
+        http_response_code(404);
+   }
 
 ?>
--- thumb.php
+++ thumb.php
@@ -4,47 +4,48 @@
     require_once('php_image_magician.php');
 
     $name = $_GET["f"];
+    $extensions    = array(".jpg",".png",".gif",".JPG",".PNG",".GIF");
+    $ext = strrchr($name, '.');
+    if(in_array($ext, $extensions)) {
 
-    // Open JPG image
-    $magicianObj = new imageLib($name);
+        // Open JPG image
+        $magicianObj = new imageLib($name);
 
-    // Resize to best fit then crop
-    $magicianObj -> resizeImage(200, 150, 'auto');
+        // Resize to best fit then crop
+        $magicianObj -> resizeImage(200, 150, 'auto');
 
-    $exifArray = $magicianObj -> getExif();
+        $exifArray = $magicianObj -> getExif();
 //  1: 'rotate(0deg)',
 //  3: 'rotate(180deg)',
 //  6: 'rotate(90deg)',
 //  8: 'rotate(270deg)'
 
-    switch ($exifArray['orientation']) {
-        case 1:
-            // $magicianObj -> rotate(0);
-             break;
-        case 3:
-             $magicianObj -> rotate(180);
-             break;
-        case 6:
-             $magicianObj -> rotate(90);
-             break;
-        case 8:
-             $magicianObj -> rotate(270);
-             break;
+        switch ($exifArray['orientation']) {
+            case 1:
+                 // $magicianObj -> rotate(0);
+                 break;
+            case 3:
+                 $magicianObj -> rotate(180);
+                 break;
+            case 6:
+                 $magicianObj -> rotate(90);
+                 break;
+            case 8:
+                 $magicianObj -> rotate(270);
+                 break;
+        }
+
+        // Save resized image as a PNG
+        $magicianObj -> displayImage('jpg',80) ;
+
+        $th_name = str_replace("albums","thumbs",$name);
+        $th_name = str_replace("//","/",$th_name);
+        if (!file_exists(dirname($th_name))) {
+            mkdir(dirname($th_name), 0777, true);
+        }
+	$magicianObj -> saveImage($th_name,85);
+    } else {
+        http_response_code(404);
     }
-
-
-    // Save resized image as a PNG
-    $magicianObj -> displayImage('jpg',80) ;
-
-
-
-    $th_name = str_replace("albums","thumbs",$name);
-    $th_name = str_replace("//","/",$th_name);
-//    echo $th_name;
-    if (!file_exists(dirname($th_name))) {
-        mkdir(dirname($th_name), 0777, true);
-    }
-    $magicianObj -> saveImage($th_name,85);
-
 
 ?>
